a77d4f574ea0dc3e944d018b54de0624a1489e3e
suddal <stine@uddal.com>
2020-07-14 10:53:51 +0200

    Add validation for duplicates

--- force-app/communities/kurs/registrationForm/lwc/courseRegistrationForm/courseRegistrationForm.js
+++ force-app/communities/kurs/registrationForm/lwc/courseRegistrationForm/courseRegistrationForm.js
@@ -1,15 +1,27 @@
 import { LightningElement, track } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 import findOrCreateContact from '@salesforce/apex/CourseRegistrationController.findOrCreateContact';
-export default class CourseRegistrationForm extends LightningElement {
+
+export default class CourseRegistrationForm extends NavigationMixin(LightningElement) {
     @track theRecord = {};
     @track output;
+
+    @track showForm = true;
+    @track showConfirmation = false;
+    @track message;
+
 
     handleChange(event) {
         this.theRecord[event.target.name] = event.target.value;
     }
     handleSubmit() {
         let output = JSON.stringify(this.theRecord, null);
-        findOrCreateContact({ fields: output });
+        findOrCreateContact({ fields: output })
+            .then(result => {
+                this.showForm = false;
+                this.showConfirmation = true;
+                this.message = result;
+                console.log('result', result);
+            });
     }
-
 }
