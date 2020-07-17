14739db69d5a78629b1fbdfd5fcbd38ac716f341
piszosta <42918948+piszosta@users.noreply.github.com>
2020-07-17 15:20:57 +0200

    LWC for Course registration information box in Community

---
+++ force-app/main/default/lwc/courseRegistrationInformation/courseRegistrationInformation.js
@@ -0,0 +1,28 @@
+import { LightningElement, track } from 'lwc';
+
+export default class courseRegistrationInformation extends LightningElement {
+
+    @track Course__c;
+    parameters = {};
+
+    connectedCallback() {
+
+        this.parameters = this.getQueryParameters();
+        this.courseId = this.parameters.id;
+
+    }
+
+    getQueryParameters() {
+
+        var params = {};
+        var search = location.search.substring(1);
+
+        if (search) {
+            params = JSON.parse('{"' + search.replace(/&/g, '","').replace(/=/g, '":"') + '"}', (key, value) => {
+                return key === "" ? value : decodeURIComponent(value)
+            });
+        }
+
+        return params;
+    }
+}
