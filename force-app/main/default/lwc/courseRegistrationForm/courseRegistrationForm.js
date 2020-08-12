96ba308d8f4d0ca0055668f50f25bbe86c8704da
suddal <stine@uddal.com>
2020-08-12 16:38:35 +0200

    Add functionallity to check validation code

--- force-app/main/default/lwc/courseRegistrationForm/courseRegistrationForm.js
+++ force-app/main/default/lwc/courseRegistrationForm/courseRegistrationForm.js
@@ -1,63 +1,98 @@
-import { LightningElement, track } from "lwc";
+import { LightningElement, track, wire } from "lwc";
 import { NavigationMixin } from "lightning/navigation";
 import createRegistration from "@salesforce/apex/CourseRegistrationController.createRegistration";
+import getInvitationCode from "@salesforce/apex/CourseRegistrationController.getInvitationCode";
+
 
 export default class CourseRegistrationForm extends NavigationMixin(
-  LightningElement
+    LightningElement
 ) {
-  @track theRecord = {};
-  @track output;
+    @track theRecord = {};
+    @track output;
 
-  @track showForm = true;
-  @track showConfirmation = false;
-  @track showError = false;
-  @track message;
+    @track showForm = false;
+    @track showConfirmation = false;
+    @track showError = false;
+    @track message;
 
-  @track courseId;
+    @track inputValCode;
+    @track code;
 
-  parameters = {};
 
-  connectedCallback() {
-    this.parameters = this.getQueryParameters();
-    this.courseId = this.parameters.id;
-  }
+    @track courseId;
 
-  getQueryParameters() {
-    var params = {};
-    var search = location.search.substring(1);
+    @track showValidationInput = false;
+    parameters = {};
 
-    if (search) {
-      params = JSON.parse(
-        '{"' + search.replace(/&/g, '","').replace(/=/g, '":"') + '"}',
-        (key, value) => {
-          return key === "" ? value : decodeURIComponent(value);
-        }
-      );
+    connectedCallback() {
+        this.parameters = this.getQueryParameters();
+        this.courseId = this.parameters.id;
+
+        this.checkValidationCode();
     }
-    return params;
-  }
 
-  handleChange(event) {
-    this.theRecord[event.target.name] = event.target.value;
-    this.showError = false;
-  }
-  handleSubmit() {
-    if (
-      this.theRecord.firstName &&
-      this.theRecord.lastName &&
-      this.theRecord.email &&
-      this.theRecord.phone
-    ) {
-      let output = JSON.stringify(this.theRecord, null);
-      createRegistration({ fields: output, courseId: this.courseId }).then(
-        result => {
-          this.showForm = false;
-          this.showConfirmation = true;
-          this.message = result;
-        }
-      );
-    } else {
-      this.showError = true;
+    checkValidationCode() {
+        getInvitationCode({ courseId: this.courseId }).then(
+            result => {
+                if (result) {
+                    this.showValidationInput = true;
+                    this.showForm = false;
+                    this.code = result;
+                } else {
+                    this.showForm = true;
+                }
+            }
+        );
     }
-  }
+
+    getQueryParameters() {
+        var params = {};
+        var search = location.search.substring(1);
+
+        if (search) {
+            params = JSON.parse(
+                '{"' + search.replace(/&/g, '","').replace(/=/g, '":"') + '"}',
+                (key, value) => {
+                    return key === "" ? value : decodeURIComponent(value);
+                }
+            );
+        }
+        return params;
+    }
+
+    handleChange(event) {
+        this.theRecord[event.target.name] = event.target.value;
+        this.showError = false;
+    }
+
+    handleChange2(event) {
+        this.inputValCode = event.target.value;
+    }
+
+    handleSubmit() {
+        if (
+            this.theRecord.firstName &&
+            this.theRecord.lastName &&
+            this.theRecord.email &&
+            this.theRecord.phone
+        ) {
+            let output = JSON.stringify(this.theRecord, null);
+            createRegistration({ fields: output, courseId: this.courseId }).then(
+                result => {
+                    this.showForm = false;
+                    this.showConfirmation = true;
+                    this.message = result;
+                }
+            );
+        } else {
+            this.showError = true;
+        }
+    }
+
+    validateCode() {
+        if (this.inputValCode === this.code) {
+            this.showValidationInput = false;
+            this.showForm = true;
+        }
+    }
 }
