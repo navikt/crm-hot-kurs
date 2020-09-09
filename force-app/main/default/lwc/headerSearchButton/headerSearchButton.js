41588875ee4fbce8e9695bfd2c1d274b83cf8794
suddal <stine@uddal.com>
2020-09-09 16:25:51 +0200

    Update css

--- force-app/main/default/lwc/headerSearchButton/headerSearchButton.js
+++ force-app/main/default/lwc/headerSearchButton/headerSearchButton.js
@@ -1,3 +1,8 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class HeaderSearchButton extends LightningElement {}
+export default class HeaderSearchButton extends LightningElement {
+    @track searchPressed = false;
+    handleOnClickSearch(event) {
+        this.searchPressed = !this.searchPressed;
+    }
+}
