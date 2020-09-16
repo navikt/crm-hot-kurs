916c702c8f014f677b67df2eaf2420098db3e385
John Arne Føreland <john.foreland@me.com>
2020-09-16 15:50:37 +0200

    one field for name

--- force-app/main/default/lwc/courseRegistrationEmailComponent/courseRegistrationEmailComponent.js
+++ force-app/main/default/lwc/courseRegistrationEmailComponent/courseRegistrationEmailComponent.js
@@ -1,5 +1,6 @@
 import { LightningElement, track, api } from 'lwc';
 import { getDataFromInputFields, validateData, emptyInputFields } from "./helper";
+import labels from "./labels";
 
 export default class CourseRegistrationEmailComponent extends LightningElement {
 
@@ -11,6 +12,7 @@ export default class CourseRegistrationEmailComponent extends LightningElement {
     @track checkboxChecked = false;
     @track viewConfirmationWindow = false;
 
+    labels = labels;
     emailRegex = '(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])';
 
     // add pills
---
+++ force-app/main/default/lwc/courseRegistrationEmailComponent/labels.js
@@ -0,0 +1,19 @@
+import name from "@salesforce/label/c.EmailPreview_name";
+import email from "@salesforce/label/c.EmailPreview_email";
+import emailValidation from "@salesforce/label/c.EmailPreview_emailValidation";
+import addRecipient from "@salesforce/label/c.EmailPreview_addRecipient";
+import goToConfirmation from "@salesforce/label/c.EmailPreview_goToConfirmation";
+import importBtn from "@salesforce/label/c.EmailPreview_import";
+import title from "@salesforce/label/c.EmailPreview_title";
+
+
+
+export default {
+    name,
+    email,
+    emailValidation,
+    addRecipient,
+    goToConfirmation,
+    importBtn,
+    title,
+}
