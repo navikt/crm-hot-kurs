02c578a2cf09e75bad03518d70c14c5c07f3b5ba
suddal <stine@uddal.com>
2020-09-21 08:38:14 +0200

    Reference static resource css instead of internal stylesheet

--- force-app/main/default/lwc/headerSearchButton/headerSearchButton.js
+++ force-app/main/default/lwc/headerSearchButton/headerSearchButton.js
@@ -1,7 +1,14 @@
 import { LightningElement, track } from 'lwc';
+import dekoratoren from '@salesforce/resourceUrl/dekoratoren';
+import { loadStyle, loadScript } from 'lightning/platformResourceLoader';
 
 export default class HeaderSearchButton extends LightningElement {
     @track searchPressed = false;
+
+    renderedCallback() {
+        loadStyle(this, dekoratoren);
+    }
+
     handleOnClickSearch(event) {
         this.searchPressed = !this.searchPressed;
     }
