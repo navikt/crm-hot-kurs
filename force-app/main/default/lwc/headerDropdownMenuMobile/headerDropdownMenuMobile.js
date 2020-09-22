af97ed5b350ad5f75ed433198adaf3196de85cc6
suddal <stine@uddal.com>
2020-09-22 13:04:22 +0200

    Add mobile menu for header

---
+++ force-app/main/default/lwc/headerDropdownMenuMobile/headerDropdownMenuMobile.js
@@ -0,0 +1,21 @@
+import { LightningElement, track } from 'lwc';
+import dekoratoren from '@salesforce/resourceUrl/dekoratoren';
+import { loadStyle, loadScript } from 'lightning/platformResourceLoader';
+
+export default class HeaderDropdownMenuMobile extends LightningElement {
+
+    @track menuPressed = false;
+
+
+    renderedCallback() {
+        loadStyle(this, dekoratoren);
+    }
+
+
+    handleOnClickMenu(event) {
+        console.log('test');
+        this.menuPressed = !this.menuPressed;
+
+        //this.sendMenuSelectedEvent();
+    }
+}
