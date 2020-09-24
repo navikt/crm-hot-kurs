a675581f7d2e804640972e13c558f20be3f88576
John Arne Føreland <john.foreland@me.com>
2020-09-24 16:59:05 +0200

    fixed bug if no email found, will still import

--- force-app/components/importUsers/lwc/importUsersModal/importUsersModal.js
+++ force-app/components/importUsers/lwc/importUsersModal/importUsersModal.js
@@ -15,7 +15,10 @@ export default class ImportUsersModal extends LightningElement {
         users.forEach(function (user, index) {
             if (user) {
                 let regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
-                let email = user.toLowerCase().match(regex)[0];
+                let email = user.toLowerCase().match(regex);
+
+                // if email found, use the first from array
+                if (email) email = email[0];
                 var emailPattern = new RegExp(email, 'gi');
                 let fullName = user.replace(emailPattern, '').replaceAll(',', '').replaceAll(';', '').replaceAll('"', '').trim();
                 this.push({ id: index, fullName: fullName, email: email });
