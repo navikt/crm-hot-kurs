75d2902c0dfce67b6ba8654f1956729ef811c8f7
John Føreland <john.foreland@soprasteria.com>
2020-11-10 09:49:17 +0100

    updated to new momentJs file path

--- force-app/main/default/lwc/courseRegistrationInformation/courseRegistrationInformation.js
+++ force-app/main/default/lwc/courseRegistrationInformation/courseRegistrationInformation.js
@@ -2,7 +2,7 @@ import { LightningElement, api, track } from 'lwc';
 import getCourseFields from "@salesforce/apex/CourseRegistrationController.getCourseFields";
 import icons from '@salesforce/resourceUrl/icons';
 import { loadScript } from 'lightning/platformResourceLoader';
-import MOMENT_JS from '@salesforce/resourceUrl/moment_js';
+import MOMENT_JS from '@salesforce/resourceUrl/momentJs';
 
 export default class courseRegistrationInformation extends LightningElement {
     @api courseId;
