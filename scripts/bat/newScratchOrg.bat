ec0077baec515fb5ccca3a2c5692265d969d6ee1
Carl Huseby Fosli <carl@fosli.net>
2023-12-15 09:24:09 +0100

    Updates (#323)
    * Update sfdx-project.json
    
    * Create newScratchOrg.bat
    
    * Update settings.json
    
    * Updates to community from prod
    
    * Updates to flexipages
    
    * Updates from prod with new api version
    
    * Update Course_Admin.permissionset-meta.xml
    
    * Updates to workflows

---
+++ scripts/bat/newScratchOrg.bat
@@ -0,0 +1,55 @@
+echo "Oppretter scratch org"
+call sf org create scratch --definition-file config\project-scratch-def.json --alias %1 --duration-days %2 --set-default --json --wait 30
+
+echo "Installerer crm-platform-base ver. 0.199"
+call sf package install --package 04t7U000000Y2UvQAK --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+
+echo "Installerer crm-platform-access-control ver. 0.116"
+call sf package install --package 04t7U000000Y2ZlQAK --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+
+echo "Installerer crm-platform-reporting ver. 0.37"
+call sf package install --package 04t7U000000Y2VoQAK --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+
+echo "Installerer crm-shared-base ver. 1.1"
+call sf package install --package 04t2o000000ySqpAAE --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+
+echo "Installerer crm-thread-view ver. 0.2"
+call sf package install --package 04t7U000000TqvIQAS --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+
+echo "Installerer crm-shared-timeline ver. 1.22"
+call sf package install --package 04t7U000000Y2OEQA0 --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+
+echo "Installerer crm-shared-flowComponents ver. 0.4"
+call sf package install --package 04t7U0000008qz4QAA --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+
+echo "Installerer crm-henvendelse-base ver. 0.18"
+call sf package install --package 04t7U000000LPPAQA4 --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+
+echo "Installerer crm-platform-integration ver. 0.106"
+call sf package install --package 04t7U000000Y2cQQAS --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+
+echo "Installerer crm-journal-utilities ver. 0.26"
+call sf package install --package 04t7U0000000RlpQAE --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+
+echo "Installerer crm-platform-oppgave ver. 0.49"
+call sf package install --package 04t7U000000D2SOQA0 --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+
+echo "Installerer crm-arbeidsgiver-base ver. 1.361"
+call sf package install --package 04t7U000000Y2aUQAS --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+
+echo "Installerer crm-community-base ver. 0.95"
+call sf package install --package 04t7U000000Y2XBQA0 --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+
+echo "Installerer crm-platform-email-scheduling ver. 1.6"
+call sf package install --package 04t2o000000yRSSAA2 --no-prompt --installation-key %3 --wait 30 --publish-wait 30
+
+echo "Dytter kildekoden til scratch org'en"
+call sf project deploy start
+
+echo "Tildeler tilatelsessett til brukeren"
+call sf org assign permset --name Kurs_Salesforce_Admin
+
+echo "Publish Experience Site"
+call sf community publish --name Kurs1
+
+echo "Ferdig"
